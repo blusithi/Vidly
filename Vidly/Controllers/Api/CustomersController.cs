fbfc3aeb0a60efc00f3a2671c48aeaad27d26674
Bonele <blusithi@yahoo.co.uk>
2017-06-24 11:31:50 +0200

    Added Customer Dto  and AutoMapper

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+
+        private ApplicationDbContext db;
+
+        public CustomersController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetCustomers()
+        {
+            return Ok(db.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>));
+        }
+
+        public IHttpActionResult GetCustomer(int Id)
+        {
+            var customer = db.Customers.SingleOrDefault(c => c.Id == Id);
+
+            if (customer == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            customerDto.Id = customer.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
+        }
+
+        [HttpPut]
+        public void UpdateCustomer(int Id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+  
+
+            var customerInDb = db.Customers.SingleOrDefault(c => c.Id == Id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(customerDto, customerInDb);
+            db.SaveChanges();
+        }
+
+        public void DeleteCustomer(int Id)
+        {
+            if(Id <= 0)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            var customerInDb = db.Customers.SingleOrDefault(c => c.Id == Id);
+            if(customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            db.Customers.Remove(customerInDb);
+            db.SaveChanges();
+        }
+
+
+
+    }
+}
--- Vidly/DTOs/CustomerDto.cs
+++ Vidly/DTOs/CustomerDto.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Vidly.Models;
 
 namespace Vidly.DTOs
 {
     public class CustomerDto
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public byte MembershipTypeId { get; set; }
+        //[Min18Years]
+        public DateTime? BirthDate { get; set; }
 
     }
 }
