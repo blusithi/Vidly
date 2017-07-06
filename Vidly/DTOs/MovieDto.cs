f59618f7c754c2018bd439fd4c54c69444a17692
Bonele Lusithi <bonele.lusithi@dariel.co.za>
2017-07-06 10:14:56 +0200

    Fixed bug on Movies api and view javascript. Refactored classes.

--- Vidly/DTOs/MovieDto.cs
+++ Vidly/DTOs/MovieDto.cs
@@ -13,6 +13,7 @@ namespace Vidly.DTOs
         [StringLength(45)]
         public string Name { get; set; }
         public byte GenreId { get; set; }
+        public GenreDto Genre { get; set; }
         [Required]
         public DateTime? ReleaseDate { get; set; }
         public DateTime? DateAdded { get; set; }
